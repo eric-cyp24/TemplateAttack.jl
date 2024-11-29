4f7cccd5cbc749341fc7d4884df9c09c0d445c36
Eric Peng <cyp24@cl.cam.ac.uk>
2024-11-29 17:18:42 +0000

    add coloring to plots in utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -2,19 +2,22 @@
 
 
 """
-    plotdatascatter!(data::AbstractMatrix; axes=[1,2], show=true, kwarg...)
+    plotdatascatter!(data::AbstractMatrix; axes=[1,2], show=true, groupdict=nothing, colors=nothing, kwarg...)
 
 Scatterplot of the data along the given two axes.
 """
-function plotdatascatter(data::AbstractMatrix; axes=[1,2], show=true, groupdict=nothing, kwarg...)
-    plot(); plotdatascatter!(data; axes, show, groupdict, kwarg...)
+function plotdatascatter(data::AbstractMatrix; axes=[1,2], show=true, groupdict=nothing, colors=nothing, kwarg...)
+    plot(); plotdatascatter!(data; axes, show, groupdict, colors, kwarg...)
 end
-function plotdatascatter!(data::AbstractMatrix; axes=[1,2], show=true, groupdict=nothing, kwarg...)
+function plotdatascatter!(data::AbstractMatrix; axes=[1,2], show=true, groupdict=nothing, colors=nothing, kwarg...)
     if isnothing(groupdict)
         x,y = eachrow(view(data,axes,:))
         p = scatter!(x, y; label="", markersize=1, markeralpha=0.7, markerstrokewidth=-1, color=:grey, kwarg...)
     else
-        for (c,l) in enumerate(sort(collect(keys(groupdict))))
+        colors = isnothing(colors) ? (1:length(groupdict)) : colors
+        labels = sort(collect(keys(groupdict)))
+        colordict = (colors isa Dict) ? colors : Dict(l=>Int(c) for (l,c) in zip(labels,colors))
+        for (l,c) in sort(colordict)
             x,y = eachrow(view(data,axes,groupdict[l]))
             p = scatter!(x, y; label="", markersize=1, markeralpha=0.7, markerstrokewidth=-1, color=c, kwarg...)
         end
@@ -52,10 +55,11 @@ end
 function plotTemplate!(t::Template; axes=[1,2], show=true, colors=nothing, kwarg...)
     colors = isnothing(colors) ? (1:length(t.mvgs)) : colors
     labels = sort(collect(keys(t.mvgs)))
-    for (c,iv) in zip(colors, labels)
-        loc, cov = t.mvgs[iv].μ[axes], t.mvgs[iv].Σ[axes,axes]
+    colordict = (colors isa Dict) ? colors : Dict(l=>Number(c) for (l,c) in zip(labels,colors))
+    for (l,c) in sort(colordict)
+        loc, cov = t.mvgs[l].μ[axes], t.mvgs[l].Σ[axes,axes]
         covellipse!(loc, cov; n_std=2.448, fillalpha=0, linealpha=1,
-                    linewidth=1, linecolor=c, label=iv, kwarg...)
+                    linewidth=1, linecolor=c, label=l, kwarg...)
         covellipse!(loc, cov; n_std=3.035, fillalpha=0, linealpha=0.5,
                     linewidth=0.7, linecolor=c, kwarg..., label="")
     end
@@ -79,10 +83,16 @@ Load from .h5 file or .npy file.
 """
 function loaddata(filename::AbstractString; datapath=nothing)
     if split(filename,".")[end] == "h5"
-        f = h5open(filename)
-        data = isnothing(datapath) ? read(f) : read(f,datapath)
-        close(f)
-        return data
+        return h5open(filename) do f
+            if isnothing(datapath)
+                return read(f)
+            else
+                dset = f[datapath]
+                # if file size > 2^28 ≈ 256 MB, then use memmap
+                return length(dset) > 2^28 && HDF5.ismmappable(dset) ? 
+                       HDF5.readmmap(dset) : read(dset)
+            end
+        end
     elseif split(filename,".")[end] == "npy" && isnpy(filename)
         # if file size > 2^28 ≈ 256 MB, then use memmap
         return loadnpy(filename; memmap=filesize(filename)>2^28, numpy_order=false)
